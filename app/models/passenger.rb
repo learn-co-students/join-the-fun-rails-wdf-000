9d97bd9e04f012d47bb483337f429a424b331400
sami4thewin <samiomarmohammed@gmail.com>
2016-09-29 14:01:51 -0400

    Done.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,8 @@
 class Passenger < ActiveRecord::Base
+
+  has_many :rides
+
+  has_many :taxis_passengers
+  has_many :taxis, through: :rides
+
 end
---
+++ db/migrate/9_create_join.rb
@@ -0,0 +1,10 @@
+class CreateJoin < ActiveRecord::Migration
+
+  def change
+    create_table :taxis_passengers do |t|
+      t.integer :taxi_id
+      t.integer :passenger_id
+    end
+  end
+
+end
